d0e833b068669ca98f1273360372c00bef7fbb09
Yael B <yael@any.do>
2023-03-01 12:56:32 +0200

    add demo data

---
+++ apps/keep/services/note.service.js
@@ -0,0 +1,13 @@
+import { storageService } from '../../../services/async-storage.service.js'
+import { utilService } from '../../../services/util.service.js'
+import demoNotes from '../../../assets/demo/keep.json' assert { type: 'json' }
+
+const KEEP_KEY = 'keepDB'
+
+export const noteService = {
+  query,
+  //   get,
+  //   post,
+  //   remove,
+  //   put,
+}
