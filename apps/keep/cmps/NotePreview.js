d344fca0bb208878fccd6183b5dd5fa533ccb474
Yael B <yael@any.do>
2023-03-02 04:00:55 +0200

    Add note preview option buttons

--- apps/keep/cmps/NotePreview.js
+++ apps/keep/cmps/NotePreview.js
@@ -10,9 +10,28 @@ export default {
   template: `
     <article v-if="note" class="note-preview" :style="background">
         <component :is="note.type" :info="note.info"/>
+
+        <section class="options">
+            <button class="options-btn" @click.prevent="duplicate" stitle="Make a copy"><i class="fa-solid fa-copy"></i></button>
+            <button class="options-btn" @click="remove" title="Delete"><i class="fa-solid fa-trash"></i></button>
+            <button class="options-btn" class="color" @click="colorPalletOpen = !colorPalletOpen" title="Change color"><i class="fa-solid fa-palette"></i></button>
+            <button class="options-btn" @click.prevent="pin" title="Pin"><i class="fa-solid fa-thumbtack"></i></button>
+            <button class="options-btn" @click="edit" title="Edit note"><i class="fa-solid fa-pencil"></i></button> 
+        </section>
+
     </article>
     `,
-  methods: {},
+  data() {
+    return {
+      colorPalletOpen: false,
+    }
+  },
+  methods: {
+    duplicate() {},
+    remove() {},
+    pin() {},
+    edit() {},
+  },
   computed: {
     background() {
       return { backgroundColor: this.note.style.backgroundColor }
