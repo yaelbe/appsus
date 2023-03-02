04afba6ffebedf1041ead74c671ba970939aa6d5
Lidor Tal <lidortald@gmail.com>
2023-03-02 10:24:39 +0200

    style Details and Add

--- apps/mail/pages/MailDetails.js
+++ apps/mail/pages/MailDetails.js
@@ -1,3 +1,4 @@
+import { router } from "../../../routes.js";
 import { mailService } from "../services/mail.service.js";
 
 export default {
@@ -6,10 +7,10 @@ export default {
   <section class="mail-details" v-if="mail">
 
     <div class="mail-details-card">
-      <h1>From: {{mail.from}}</h1>
+      <button class="mail-details-btn" @click="backToMain" >x</button>
+      <h1 class="from">From: {{mail.from}}</h1>
       <h2>Subject: {{mail.subject}}</h2>
       <p>{{mail.body}}</p>
-
     </div>
 
   </section>
@@ -25,6 +26,9 @@ export default {
     loadMail() {
       const { mailId } = this.$route.params;
       mailService.get(mailId).then((mail) => (this.mail = mail));
+    },
+    backToMain() {
+      router.push(`/mail/`);
     },
   },
   computed: {},
